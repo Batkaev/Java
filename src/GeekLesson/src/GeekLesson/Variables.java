402d7213b66721832821adce98a30f3b69608a5e
Batkaev Dmitriy <dv_batkaev@rambler.ru>
2021-03-05 23:26:59 +0300

    Задание на создание и инициализацие переменных

---
+++ src/GeekLesson/src/GeekLesson/Variables.java
@@ -0,0 +1,23 @@
+package GeekLesson;
+
+public class Variables {
+    public static void main(String[] args) {
+        int a;
+        byte b;
+        short s;
+        long l;
+        float f;
+        double d;
+        char c;
+        boolean k;
+        a=1;
+        b = 2;
+        s = 1;
+        l = 456;
+        f = (float) 25.6;
+        d = 12.5;
+        c = 'A';
+        k = false;
+
+    }
+}
