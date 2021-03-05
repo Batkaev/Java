c2dafb0b079ae4aa3d01c48edabbe4f916e95b48
Batkaev Dmitriy <dv_batkaev@rambler.ru>
2021-03-05 23:43:50 +0300

    Написать метод, вычисляющий выражение a * (b + (c / d)) и возвращающий результат,

---
+++ src/GeekLesson/src/GeekLesson/Calculate.java
@@ -0,0 +1,13 @@
+package GeekLesson;
+
+public class Calculate {
+
+
+    public static void main(String[] args) {
+        float a=1;
+        float b=2;
+        float c=3;
+        float d=4;
+        System.out.println( a*(b+(c/d)));
+    }
+}
